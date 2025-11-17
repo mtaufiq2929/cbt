d39eef92b7bb3f9b441309c1f890865ef4eadf88
mtaufiq2929 <m.taufiq2929@admin.smk.belajar.id>
2025-11-17 20:36:59 +0700

    a

--- assets/plugins/summernote/plugin/gallery/src/index.js
+++
@@ -1,9 +0,0 @@
-import GalleryPlugin from './Module'
-
-var gallery_plugin = new GalleryPlugin({
-    name: 'gallery',
-    tooltip: 'Gallery',
-});
-
-// add the plugin to summernote
-$.extend($.summernote.plugins, gallery_plugin.getPlugin());
